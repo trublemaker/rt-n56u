6453e1ef9ce0ae5790543164334c2bcf437b6dba
mao <mao@6>
2020-05-01 13:17:21 +0800

    add ANTFS udpxy remove log info.

--- trunk/tools/depmod.sh
+++ trunk/tools/depmod.sh
@@ -42,6 +42,18 @@ if [ "$CONFIG_FIRMWARE_INCLUDE_SFE" = "y" ] ; then
 	cp -f "$sfe_dir/"*.ko "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/sfe"
 fi
 
+#if [ "$CONFIG_FIRMWARE_INCLUDE_SFE" = "y" ] ; then
+	#sfe_dir="${ROOTDIR}/user/shortcut-fe/shortcut-fe"
+	#mkdir -p "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/sfe"
+	#cp -f "$sfe_dir/"*.ko "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/sfe"
+#fi
+
+if [ "$CONFIG_FIRMWARE_INCLUDE_ANTFS" = "y" ] ; then
+	antfs_dir="${ROOTDIR}/user/antfs"
+	mkdir -p "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/antfs"
+	cp -f "$antfs_dir/"antfs.ko "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/antfs"
+fi
+
 # call depmod
 sudo $depmod_bin -ae -F System.map -b "${INSTALL_MOD_PATH}" -r ${KERNELRELEASE}
 
